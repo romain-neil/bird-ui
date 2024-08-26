5481f2e05045ac827ba32513b1fa0b9665bd40d0
Romain Neil <contact@romain-neil.fr>
2024-08-26 18:35:20 +0200

    feat: interface model

---
+++ src/models/Interface.h
@@ -0,0 +1,16 @@
+//
+// Created by Romain on 26/08/2024.
+//
+
+#ifndef BIRD_UI_INTERFACE_H
+#define BIRD_UI_INTERFACE_H
+
+#include <QString>
+
+struct Interface {
+	QString name;
+	QString type;
+	int mtu;
+};
+
+#endif //BIRD_UI_INTERFACE_H
